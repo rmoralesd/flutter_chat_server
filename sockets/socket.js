271b8ae61e63869a8e715f9f64e5fac83af178c9
Ricardo Morales Domínguez <rmorales@metrofflex.com>
2021-06-30 19:42:15 -0500

    Server working

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,7 +1,19 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Aerosmith'));
+bands.addBand(new Band('Otra cosa'));
+bands.addBand(new Band('Otra Banda'));
+
+
 //Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands());
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
@@ -11,4 +23,27 @@ io.on('connection', client => {
         console.log('Mensaje', payload);
         io.emit('mensaje', { admin: 'Nuevo mensaje' });
     });
+
+    client.on('emitir-mensaje', (payload) => {
+        //io.emit('nuevo-mensaje', payload); //emite a todos, incluyendo a si mismo
+        console.log(payload);
+        client.broadcast.emit('nuevo-mensaje', payload);
+
+    });
+
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('add-band', (payload => {
+        console.log(payload);
+        bands.addBand(new Band(payload.name));
+        io.emit('active-bands', bands.getBands());
+    }));
+
+    client.on('delete-band', (payload => {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    }));
 });
